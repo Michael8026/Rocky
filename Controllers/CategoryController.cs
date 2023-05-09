39a1a53333bc69410b5a1d8973b8767268cd612c
m.sogunro <m.sogunro@lagetronix.com>
2023-05-09 10:01:41 +0100

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _context.Category;
+
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Category.Add(obj);
+
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+            
+
+            
+        }
+    }
+}
